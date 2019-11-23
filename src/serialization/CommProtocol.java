b50eda5151b5bfe5fc93372a56a2a1e7a638bc0f
DanielCorreia21 <LostTimesEraProject@users.noreply.github.com>
2019-11-23 22:30:53 +0000

    a

--- src/serialization/CommProtocol.java
+++ src/serialization/CommProtocol.java
@@ -1,10 +1,17 @@
 package serialization;
 
-public class CommProtocol {
+import java.io.Serializable;
 
-	public CommProtocol() {
-		
-		
+public class CommProtocol implements Serializable{
+
+	private int function;
+	private boolean value;
+	private int[][] table;
+	private int[] initialPos;
+	private int [][] availablePos;
+	
+	private CommProtocol(int function) {
+		this.function = function;
 	}
 
 }
